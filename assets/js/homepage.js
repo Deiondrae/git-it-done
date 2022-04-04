64ecf5ca02ae92a0a4f476dacef95fe86cc855e1
Deiondrae <deiondrae@gmail.com>
2022-04-03 21:50:25 -0400

    page redirecting to homepage issue

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -53,7 +53,7 @@ var displayRepos = function(repos, searchTerm){
         //create a container for each repo
         var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
-        repoEl.setAttribute("href", "../../single-repo.html");
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
         //create a span eleemnt to hold repo anme
         var titleEl = document.createElement("span");
         titleEl.textContent = repoName;

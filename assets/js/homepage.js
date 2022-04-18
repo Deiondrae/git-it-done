7922074adc512e4c0bd598760c57ea8993249cd4
Villager <deiondrae@gmail.com>
2022-04-18 16:15:47 -0400

    added searching by language functionality

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 var getUserRepos = function(user){
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -80,4 +81,27 @@ var displayRepos = function(repos, searchTerm){
         repoContainerEl.appendChild(repoEl)
     }
 };
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues"
+    
+    fetch(apiUrl).then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                displayRepos(data.items, language)
+                console.log(data.items)
+            });
+        } else {
+            alert('Error: Github User Not Found')
+        }
+    })
+};
+var buttonClickHandler = function(event){
+    var language = event.target.getAttribute("data-language")
+    if(language){
+        getFeaturedRepos(language);
+
+        repoContainerEl.textContent = "";
+    }
+}
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler)
